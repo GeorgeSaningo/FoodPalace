48fd5754c37e6fce3cd2a48e8fd9ba9aecc85829
GeorgeSaningo <jorgesaningo@gmail.com>
2023-11-29 16:32:33 +0300

    Initial Commit

---
+++ mainapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return None
+
+
+def menu(request):
+    return None
+
+
+def events(request):
+    return None
+
+
+def chefs(request):
+    return None
+
+
+def contact(request):
+    return None
+
+
+def book_table(request):
+    return None
+
+
+def gallery(request):
+    return None
+
+
+def specials(request):
+    return None
