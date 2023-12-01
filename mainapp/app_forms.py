9a0512657841d829a96a80b2e8e40f151f0369fd
GeorgeSaningo <jorgesaningo@gmail.com>
2023-12-01 22:05:25 +0300

    Revised Commit

---
+++ mainapp/app_forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from mainapp.models import Booking
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        widgets = {
+            "date": forms.DateTimeInput(attrs={"type": "date", "min": "2023-01-01", "max": "2025-12-31"}),
+            "number_of_people": forms.NumberInput(attrs={"max": 30, "min": 1})
+        }
+        labels = {
+            "dateandtime": "Date and Time ",
+            "email": "Email Address"
+        }
+
+
+class LoginForm(forms.Form):
+    name = forms.CharField(max_length=60)
+    email = forms.CharField(max_length=60)
